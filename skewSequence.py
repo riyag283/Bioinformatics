39fc5125b3ceb1ef90d539087e4d013676c61ad5
Riya Gupta <riyag2803@gmail.com>
2020-09-29 17:39:58 +0530

    Update skewSequence.py

--- skewSequence.py
+++ skewSequence.py
@@ -10,3 +10,8 @@ def skew(genome):
 
 genome = input('Enter Genome: ')
 print(skew(genome))
+
+'''
+Enter Genome: CATTCCAGTACTTCATGATGGCGTGAAGA
+[0, -1, 0, 0, 0, -1, -2, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, 1, 0, 0, 1, 2, 1, 2, 0, 1, 0, 0, 1]
+'''
