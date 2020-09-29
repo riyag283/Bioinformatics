4c49c0e791f44bd94a3e0bc2bd0b68ec4dbcc8af
Riya Gupta <riyag2803@gmail.com>
2020-09-29 17:56:33 +0530

    Update skewSequence.py

--- skewSequence.py
+++ skewSequence.py
@@ -2,6 +2,7 @@ def skew(genome):
     n = len(genome)
     skew = [0]*n
     for i in range(1, n):
+        skew[i] = skew[i-1]
         if genome[i-1] == 'G':
             skew[i] = skew[i-1] + 1
         elif genome[i-1] == 'C':
@@ -13,5 +14,5 @@ print(skew(genome))
 
 '''
 Enter Genome: CATTCCAGTACTTCATGATGGCGTGAAGA
-[0, -1, 0, 0, 0, -1, -2, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, 1, 0, 0, 1, 2, 1, 2, 0, 1, 0, 0, 1]
+[0, -1, -1, -1, -1, -2, -3, -3, -2, -2, -2, -3, -3, -3, -4, -4, -4, -3, -3, -3, -2, -1, -2, -1, -1, 0, 0, 0, 1]
 '''
